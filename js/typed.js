b91b4c1288f11f9661088fa766b111a0b00a9430
Joshua Kim <jk8ry@virginia.edu>
2019-09-30 00:46:21 -0700

    Adding hanja name

--- js/typed.js
+++ js/typed.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   var typed = new Typed('.typed', {
-    strings: ["안녕하십니까, 김건호입니다.", "Hi, I'm Josh."],
+    strings: ["Hi, I'm Josh.", "안녕하십니까, 김건호입니다.", "안녕하십니까, 金建浩입니다."],
     loop: true,
-    typeSpeed: 20,
+    typeSpeed: 80,
     backSpeed: 40,
     backDelay: 5000,
   });
