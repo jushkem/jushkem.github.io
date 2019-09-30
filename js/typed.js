b10e2a82a21bbb8b55e4e56b7f347be549419d42
Joshua Kim <jk8ry@virginia.edu>
2019-09-30 00:37:28 -0700

    Adding typing animations

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var typed = new Typed('.typed', {
+    strings: ["안녕하십니까, 김건호입니다.", "Hi, I'm Josh."],
+    loop: true,
+    typeSpeed: 20,
+    backSpeed: 40,
+    backDelay: 5000,
+  });
+});
