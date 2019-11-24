400664026f8b5d1ca6f2754bd3edc496f7662a6e
Joshua Kim <jk8ry@virginia.edu>
2019-11-24 00:54:26 -0800

    Removing stupid things

--- js/typed.js
+++ js/typed.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var typed = new Typed('.typed', {
-    strings: ["Hi, I'm Josh.", "안녕하십니까, 김건호입니다.", "安寧하십니까, 金建浩입니다."],
+    strings: ["Hi, I'm Josh."],
     loop: true,
     typeSpeed: 80,
     backSpeed: 40,
